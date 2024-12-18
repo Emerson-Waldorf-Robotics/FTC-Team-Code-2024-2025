4f8adb210597f8df1aee3c1578a6c674be701f87
Jacob Freeman <t00niyf5@duck.com>
2024-12-18 12:54:01 -0500

    Commit my current changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunMotor.java
@@ -9,17 +9,29 @@ public class RunMotor extends LinearOpMode
 {
     private DcMotor motor = null;
     double moved = 0;
+    int degrees = 20;
 
     void deg30(double power){
-        int cpos = motor.getCurrentPosition();
+        int spos = motor.getCurrentPosition();
+        int cpos;
+        int last = 0;
+
         motor.setPower(power);
         while (opModeIsActive()){
-            if (Math.abs(motor.getCurrentPosition() - cpos) > 30){
+            cpos = motor.getCurrentPosition();
+            //if (Math.abs(cpos - last) < 1){
+            //    break;
+            //}
+            moved = Math.abs(cpos - spos);
+            if (moved > degrees){
                 break;
             }
+
+            telemetry.addData("Moved", "%f/%d", moved, degrees);
+            telemetry.update();
+            sleep(20);
         }
         motor.setPower(0);
-        moved = Math.abs(motor.getCurrentPosition() - cpos);
     }
 
     @Override public void runOpMode()
@@ -32,16 +44,13 @@ public class RunMotor extends LinearOpMode
         // to 'get' must match the names assigned during the robot configuration.
         // step (using the FTC Robot Controller app on the phone).
         motor  =  hardwareMap.get(DcMotor.class, "test_motor");
-        // Reset the encoder
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         float speed = 0;
         // Last button states
         boolean lb = false, lb2 = false;
-        boolean running = false;
 
-        float motorSpeed = 0.6f;
+        float motorSpeed = 1f;
 
         telemetry.addData(">", "Touch START to start OpMode");
         telemetry.update();
@@ -51,7 +60,7 @@ public class RunMotor extends LinearOpMode
             {
                 if (gamepad1.a && !lb2) {
                     lb2 = true;
-                    motor.setTargetPosition(motor.getCurrentPosition()-30);
+                    deg30(-motorSpeed);
                     continue;
                 }
                 if (!gamepad1.a) {
@@ -61,7 +70,7 @@ public class RunMotor extends LinearOpMode
             {
                 if (gamepad1.b && !lb) {
                     lb = true;
-                    motor.setTargetPosition(motor.getCurrentPosition()+30);
+                    deg30(motorSpeed);
                     continue;
                 }
                 if (!gamepad1.a) {
@@ -72,7 +81,6 @@ public class RunMotor extends LinearOpMode
             // Tell the driver what we see, and what to do.
             telemetry.addData("Motor Position", "%d", motor.getCurrentPosition());
             telemetry.addData("Motor Power", "%f", motor.getPower());
-            telemetry.addData("Moved", moved);
             telemetry.update();
 
             sleep(10);
