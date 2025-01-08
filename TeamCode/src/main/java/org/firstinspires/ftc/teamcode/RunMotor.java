ca4d9a7e560200e18a36b9b909ba218fbf68e68b
Jacob Freeman <t00niyf5@duck.com>
2025-01-08 12:20:18 -0500

    Remove a warning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunMotor.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import static java.lang.System.exit;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
